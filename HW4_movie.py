3c2832b95162d8111d0269416a1de750b4bb1c46
MotoHuang <108415010+MotoHuang@users.noreply.github.com>
2022-07-13 16:36:21 +0800

    Update HW4_movie.py

--- HW4_movie.py
+++ HW4_movie.py
@@ -10,9 +10,6 @@ conn = sqlite3.connect('movie_info.db')
 # COMING_TIME         TEXT    NOT NULL);''')
 
 movie = conn.cursor()
-movie_name = "中文片名:"
-movie_eng_name = "英文片名:"
-movie_res_time = "上映日期:"
 response = requests.get("https://movies.yahoo.com.tw/movie_intheaters.html")
 movie_list=[]
 soup = BeautifulSoup(response.text, 'lxml')
@@ -21,7 +18,7 @@ for item in info_items:
     name = item.find('div', 'release_movie_name').a.text.strip()
     english_name = item.find('div', 'en').a.text.strip()
     release_time = item.find('div', 'release_movie_time').text.split('：')[1].strip()
-    movie_info = dict(movie_name = name, movie_eng_name = english_name, movie_res_time = release_time)
+    movie_info = dict(中文片名 = name, 英文片名 = english_name, 上映日期 = release_time)
     movie_list.append(movie_info)
     movie.execute('INSERT INTO MOVIE_INFO VALUES(?,?,?)', (name,english_name,release_time))
     conn.commit()
