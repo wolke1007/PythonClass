c760901464061ba4b7e18f3edfba6b10af869797
MotoHuang <108415010+MotoHuang@users.noreply.github.com>
2022-07-13 16:33:47 +0800

    Update HW4_movie.py

--- HW4_movie.py
+++ HW4_movie.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-#conn = sqlite3.connect('movie_info.db')
-"""
-conn.execute('''CREATE TABLE MOVIE_INFO
-(MOVIE_CHINESE_NAME TEXT    NOT NULL,
-MOVIE_FOREIGN_NAME  TEXT    NOT NULL,
-COMING_TIME         TEXT    NOT NULL);''')
-"""
-#movie = conn.cursor()
+conn = sqlite3.connect('movie_info.db')
+
+# conn.execute('''CREATE TABLE MOVIE_INFO
+# (MOVIE_CHINESE_NAME TEXT    NOT NULL,
+# MOVIE_FOREIGN_NAME  TEXT    NOT NULL,
+# COMING_TIME         TEXT    NOT NULL);''')
+
+movie = conn.cursor()
 movie_name = "中文片名:"
 movie_eng_name = "英文片名:"
 movie_res_time = "上映日期:"
@@ -23,8 +23,8 @@ for item in info_items:
     release_time = item.find('div', 'release_movie_time').text.split('：')[1].strip()
     movie_info = dict(movie_name = name, movie_eng_name = english_name, movie_res_time = release_time)
     movie_list.append(movie_info)
-#    movie.execute('INSERT INTO MOVIE_INFO VALUES(?,?,?)', (name,english_name,release_time))
-#    conn.commit()
+    movie.execute('INSERT INTO MOVIE_INFO VALUES(?,?,?)', (name,english_name,release_time))
+    conn.commit()
 #    print('{}({}) 上映日：{}'.format(name, english_name, release_time))
 
 print(movie_list)
