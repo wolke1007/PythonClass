69b385d189995efe123e69716ec6246d171596a5
MotoHuang <108415010+MotoHuang@users.noreply.github.com>
2022-08-10 15:51:36 +0800

    Update Biggo爬蟲.py

--- Biggo爬蟲.py
+++ Biggo爬蟲.py
@@ -17,10 +17,9 @@ options.add_argument("--disable-notifications")
 
 driver = webdriver.Chrome('./chromedriver', chrome_options=options)
 
-ECs = {  # 所有通路
+ECs = {  # 所有通路(momo除外)
     'CSM EC' : 'tw_ec_cosmed',
     'WTS EC' : 'tw_ec_watsons',
-    'momo': "tw_pec_momoshop",
     'pchome':  "tw_ec_pchome24h",
     'Books' : 'tw_pec_books'
 }
@@ -33,10 +32,15 @@ def get_url_by_goods_name(goods_name, ec):
     url = f'{biggo_base_url}{goods_name}/?&m=cp&c[]={EC}'
     return url
 
+def get_momo_url_by_goods_name(goods_name):
+    """組成類似像是 https://biggo.com.tw/s/biore%20%E6%B4%97%E9%9D%A2%E4%B9%B3/?&m=cp&c[]=tw_pec_momoshop 的格式"""
+    url = f'{biggo_base_url}{goods_name}/?&m=cp&c[]=tw_pec_momoshop'
+    return url
+
 
 def wait_until(xpath: str):
-    element = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(("xpath", xpath))
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, xpath))
     )
     return element
 
@@ -44,6 +48,7 @@ def wait_until(xpath: str):
 def get_goods_name(item_name):
     goods_name = f'{item_name}'
     return goods_name
+
 
 
 def get_goods_info(url):
@@ -59,7 +64,6 @@ def get_goods_info(url):
     soup = BeautifulSoup(response.text, 'lxml')
 
     total_items = soup.find_all('div', 'col-12 product-row')
-    var = "1"
     htmlparser = etree.HTMLParser()
     #url = driver.current_url
     response = urlopen(driver.current_url) #原本是url
@@ -67,9 +71,10 @@ def get_goods_info(url):
     #xpathselector = "(//div[@class='d-flex w100'])[1]//div[@class='multple_spec_btn _dropdown ml10']"  # 第一個品項的下拉選單
     for index, each_item in enumerate(total_items, start=1):
         str_index = str(index)
-        btn_dropdowns = tree.xpath("(//div[@class='d-flex w100'])[" + str_index + "]//div[@class='multple_spec_btn _dropdown ml10']")
+        xpathselector = "(//div[@class='d-flex w100'])[" + str_index + "]//div[@class='multple_spec_btn _dropdown ml10']"
+        btn_dropdowns = tree.xpath(xpathselector)
         if btn_dropdowns != []:
-            muti_dropdown_btn = wait_until("(//div[@class='d-flex w100'])[" + str_index + "]//div[@class='multple_spec_btn _dropdown ml10']")
+            muti_dropdown_btn = wait_until(xpathselector)
             action.move_to_element(muti_dropdown_btn)
             muti_dropdown_btn.click()
             time.sleep(0.5)
@@ -99,27 +104,34 @@ def get_goods_info(url):
                 cosmetic_info = dict(品名=name, 價格=price, 通路=EC)
                 cosmetic_list.append(cosmetic_info)
 
-    # for i in cosmetic_list:
-    #     print(i)
-    # print(cosmetic_list)
     return EC, name, price
 
-# input? user 要如何提供 ec brand item_name 這些資訊
-# output? 請 user 提供 excel sample
-
-# 當多個 ec 時要如何處理
-
 items = []
-for goods_index in range(1,len(df.index)):      #取出所有品名
-    goods_name = df.at[goods_index, '中文名']
-    items.append(goods_name)
+def get_items_name_from_df(df):
+    for goods_index in range(1,len(df.index)):      #取出所有品名
+        goods_name = df.at[goods_index, '中文名']
+        items.append(goods_name)
+    return items
+
+items_momo=[]
+def get_items_name_from_df_for_momo(df):
+    for goods_index in range(1,len(df.index)):      #取出所有品名
+        goods_name = df.at[goods_index, '中文名']
+        items_momo.append(goods_name[0:-7])
+    return items_momo
 
 try:
+    get_items_name_from_df(df)
     for item_name in items:
         for ec in ECs:
             url = get_url_by_goods_name(item_name, ec)
             EC, name, price = get_goods_info(url)
-            print(goods_name, EC, price)
+            print(name, EC, price)
+    get_items_name_from_df_for_momo(df)
+    for item_name in items_momo:
+        url = get_momo_url_by_goods_name(item_name)
+        EC, name, price = get_goods_info(url)
+        print(name, EC, price)
 
 finally:
     driver.close()
